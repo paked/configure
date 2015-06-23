643a583878d0c630e17e3c50545ba2bbc982a38e
Harrison <harrison@theshoebridges.com>
2015-06-23 08:50:29 +0400

    Fix package documentation and Checker descriptions

--- doc.go
+++ doc.go
@@ -1,10 +1,11 @@
 // Package configeur is a multi-layer configuration interface.
 //
 // configeur is based around the idea of middlewear, and is completely pluggable with
-// anything that will fulfill the configeur.Checker interface. There are two built in Checkers,
-// which can optionally be added to a configeur instance's stack. They are: Environment and JSON.
-// Environment reads configuration values from your environment variables, and JSON will retrieve
-// values from a JSON string provided through an io.Reader.
+// anything that will fulfill the configeur.Checker interface. There are three built in Checkers,
+// which can optionally be added to a configeur instance's stack. They are: Environment, Flag and JSON.
+// Environment reads configuration values from your environment variables,
+// Flag will parse your os.Args into and JSON will retrieve values from a JSON
+// map provided through an io.Reader.
 //
 // As well as this, configeur aims to have a familiar API, which is why it mirrors the flag packages
 // API as closely as possible.
