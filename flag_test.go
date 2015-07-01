3c0fe2d3bfa96f65da18b63a2aec34a84b32dc62
Harrison <harrison@theshoebridges.com>
2015-07-01 14:05:11 +0100

    Add updated testing for Flag using the new suite

--- flag_test.go
+++ flag_test.go
@@ -1,22 +1,37 @@
 package configeur
 
 import (
-	"fmt"
 	"testing"
 )
 
-func TestFlags(t *testing.T) {
+func TestFlagStrings(t *testing.T) {
+	f, ff := createFlag(t, "--x=hello", `--z=hello world`, "--y=22")
+
+	test(value(f.String("x")), "hello", ff)
+	test(value(f.String("z")), "hello world", ff)
+	test(value(f.String("y")), "22", ff)
+}
+
+func TestFlagsInts(t *testing.T) {
+	f, ff := createFlag(t, "--x=2", "--z=-1")
+
+	test(value(f.Int("x")), 2, ff)
+	test(value(f.Int("z")), -1, ff)
+}
+
+func TestFlagsBools(t *testing.T) {
+	f, ff := createFlag(t, "--x=T", "--z=F")
+
+	test(value(f.Bool("x")), true, ff)
+	test(value(f.Bool("z")), false, ff)
+}
+
+func createFlag(t *testing.T, args ...string) (Flag, func()) {
 	f := Flag{
-		args: []string{"exec", "--x=2", "--y=four five six seven"},
+		args: append([]string{"executable"}, args...),
 	}
 
-	if v, _ := f.String("x"); v != "2" {
-		fmt.Println(v)
-		t.Fail()
-	}
+	ff := normalFailFunc(t)
 
-	if v, _ := f.String("y"); v != "four five six seven" {
-		fmt.Println(v)
-		t.Fail()
-	}
+	return f, ff
 }
