2f279911ff6a6ce10a6796aa7fa240e6dc3ba335
Harrison <harrison@theshoebridges.com>
2015-07-04 12:10:58 +0200

    Update information in README

--- doc.go
+++ doc.go
@@ -1,15 +1,21 @@
 // Package configeur is an easy to use multi-layer configuration system.
 //
+// Examples can be found in the example folder (http://github.com/paked/configeur/blob/master/examples/)
+// as well as a getting started guide in the main README file (http://github.com/paked/configeur).
+//
 // configeur makes use of Checkers, which are used to retrieve values from their respective data sources.
 // There are three built in Checkers, Environment, Flag and JSON. Environment retrieves environment variables.
 // Flag retrieves variables within the flags of a command. JSON retrieves values from a JSON file/blob. Checkers
-// can be essentially thought of as "configuration middlewear", in fact parts of the package API was inspired by
-// negroni (https://github.com/codegangsta/negroni, special thanks to codegangsta for the awesome package!) and the
-// standard libraries flag package.
+// can be essentially thought of as "middlewear for configuration", in fact parts of the package API was inspired by
+// negroni (https://github.com/codegangsta/negroni, the awesome net/http middlewear manager) and the standard library's flag package.
 //
 // It is very easy to create your own Checkers, all they have to do is satisfy the Checker interface.
-// That is a, Int method, String method and a Bool method. To retrieve their respective data types.
-// If you do create your own Checkers I would be more than happy to add a link to them somewhere in the repository.
+//  type Checker interface {
+//	  Int(name string) (int, error)
+// 	  Bool(name string) (int, error)
+// 	  String(name string) (string, error)
+//  }
+// That is an, Int method, String method and a Bool method. These functions are used to retrieve their respective data types.
+// If you do create your own Checkers I would be more than happy to add a link to the README in the github repository.
 //
-// A standard use-case can be found in the example folder and a getting started guide can be found on github (http://github.com/paked/configeur).
 package configeur
